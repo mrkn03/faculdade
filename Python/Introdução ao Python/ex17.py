a9b525a49fa10a30880af8aab255392a9ebd42b8
mrkn03 <128204238+mrkn03@users.noreply.github.com>
2024-04-26 11:46:53 -0300

    Update ex17.py

--- Python/Introdução ao Python/ex17.py
+++ Python/Introdução ao Python/ex17.py
@@ -1,5 +1,6 @@
 '''Faça um programa para calcular e apresentar o volume de uma lata de óleo, utilizando a fórmula volume=3.14159*raio*raio*altura. Identifique na formula os valores de entrada de dados
 e leia-os via teclado'''
+
 raio=float(input('Informe o raio da lata de óleo em centimetros: ')) # Valor de entrada
 altura=float(input('Informe a altura da lata de óleo em centimetros: ')) # Valor de entrada
 volume=(3.14159*raio*raio)*altura # Valor de saída
