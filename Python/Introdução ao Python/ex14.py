e8c7267d1c127c46be90a51abee4b7973b1f9916
mrkn03 <128204238+mrkn03@users.noreply.github.com>
2024-04-26 11:46:31 -0300

    Update ex14.py

--- Python/Introdução ao Python/ex14.py
+++ Python/Introdução ao Python/ex14.py
@@ -1,4 +1,5 @@
 '''Faça um algoritmo onde devera ser informado a base e a altura de um triangulo. o algoritmo devera exibir o valor da area'''
+
 base=float(input('Informe a base do triangulo em centimetros:  '))
 altura=float(input('Informe a altura do triangulo em centimetros:  '))
 area=(base*altura)/2
