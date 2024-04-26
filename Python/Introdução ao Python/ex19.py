7cf4fecf5fe1c9d6f28be1c9f2bfe46fcd2cfd1a
mrkn03 <128204238+mrkn03@users.noreply.github.com>
2024-04-26 11:47:10 -0300

    Update ex19.py

--- Python/Introdução ao Python/ex19.py
+++ Python/Introdução ao Python/ex19.py
@@ -1,5 +1,6 @@
 '''Escrever um algoritmo que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas por ele no mês (em dinheiro). Sabendo que este vendedor ganha 15% de comissão
 sobre suas vendas efetuadas, informar ao final do programa o seu nome, o salário fixo e salário no final do mês.'''
+
 nome=str(input('Informe o nome do vendedor: '))
 salario_fixo=float(input('Informe o salário fixo do vendedor: R$'))
 vendas=float(input('Informe o valor de vendas efetuadas no mês: R$'))
