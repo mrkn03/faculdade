ff8bc73d88727229dd7764656e04b16e7f17ab5e
mrkn03 <128204238+mrkn03@users.noreply.github.com>
2024-04-26 11:46:45 -0300

    Update ex16.py

--- Python/Introdução ao Python/ex16.py
+++ Python/Introdução ao Python/ex16.py
@@ -1,5 +1,6 @@
 '''Leia 2 valores para as variaveis A e B, efetuar a troca dos valores de forma que a variavel A passe a possuir o valor da variavel B, e que a variavel B passe a possuir o valor
 da variavel A. Apresentar os valores trocados pelas variaveis.'''
+
 a=int(input('Digite um valor qualquer: '))
 b=int(input('Digite outro valor qualquer: '))
 print('O valor de A é igual a {} e o valor de B é igual a {}.'.format(a,b))
