11a096a60f6de64112508f3f973f539aac584e95
mrkn03 <128204238+mrkn03@users.noreply.github.com>
2024-04-26 11:46:13 -0300

    Update ex12.py

--- Python/Introdução ao Python/ex12.py
+++ Python/Introdução ao Python/ex12.py
@@ -1,5 +1,6 @@
 '''Escreva um algorítmo para determinar o consumo médio de um automóvel sendo que será fornecida via teclado a distancia total percorrida pelo automóvel e o total de combustível 
 gasto'''
+
 distancia=float(input('Digite o total de km percorridos '))
 combustivel=float(input('Digite o total de combustivel consumido em litros: '))
 media=distancia/combustivel
