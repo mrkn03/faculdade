04bb2f6c686e187aff2f2d956ae49dc7da6a8c93
mrkn03 <128204238+mrkn03@users.noreply.github.com>
2024-05-07 15:24:54 -0300

    Update exercicio7For.py

--- Python/Estruturas de Repetição/exercicio7For.py
+++ Python/Estruturas de Repetição/exercicio7For.py
@@ -1,2 +1,8 @@
 '''Escreva um algoritmo que leia um valor inicial (A – A tem que ser inteiro e positivo),
 calcule a soma dos números de 1 até A e mostre o seu resultado.'''
+valor = int(input('Digite um valor inteiro qualquer: '))
+soma = 0
+for i in range(1,valor):
+    soma = valor
+    soma = soma + (1 * valor)
+print('A soma de 1 até chegar a {} é igual a {}'.format(valor,soma))
