1b8a2984ea2e486858054c23affa946cedd66d96
mrkn03 <128204238+mrkn03@users.noreply.github.com>
2024-05-07 15:25:09 -0300

    Update exercicio7While.py

--- Python/Estruturas de Repetição/exercicio7While.py
+++ Python/Estruturas de Repetição/exercicio7While.py
@@ -1,2 +1,9 @@
 '''Escreva um algoritmo que leia um valor inicial (A – A tem que ser inteiro e positivo),
 calcule a soma dos números de 1 até A e mostre o seu resultado.'''
+valor = int(input('Digite um valor inteiro qualquer: '))
+cont = 0
+soma = 0
+while cont < valor:
+    soma = valor
+    soma = soma + (1 * valor)
+print('A soma de 1 até chegar no valor {} é igual a {}'.format(valor,soma))
